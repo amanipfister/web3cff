e88a627a916a5c1be1e437f11a58eca304adee7f
amanipfister <aap.pfister@bluewin.ch>
2023-01-31 10:22:14 +0100

    NavBar component, button to create campaign, logo import and display

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
-const CustomButton = () => {
-  return <div>CustomButton</div>;
+const CustomButton = ({ btnType, title, handeClick, styles }) => {
+  return (
+    <div>
+      <button
+        type={btnType}
+        className={`font-epilogue font-semibold text-[16px] leading-[26px] text-white min-h-[52px] px-4 rounded-[10px] ${styles}`}
+        onClick={handeClick}
+      >
+        {title}
+      </button>
+    </div>
+  );
 };
 
 export default CustomButton;
