853fdf9f2d8b68709cf4a834c5b11d5b6c2bca69
amanipfister <aap.pfister@bluewin.ch>
2023-02-01 15:42:02 +0100

    add: Statecontext to contracts, add: link context to contracts, add: link campaigns ot context

---
+++ src/context/index.jsx
@@ -0,0 +1,54 @@
+import React, { useContext, createContext } from "react";
+import {
+  useAddress,
+  useContract,
+  useMetamask,
+  useContractWrite,
+} from "@thirdweb-dev/react";
+import { ethers } from "ethers";
+import { createCampaign } from "../assets";
+
+const StateContext = createContext();
+
+export const StateContextProvider = ({ children }) => {
+  const { contract } = useContract(
+    "0xE8F4ae884Dd51cA7c64525a1E4810255539a0330"
+  );
+  const { mutateAsync: createCampaign } = useContractWrite(
+    contract,
+    "createCampaign"
+  );
+
+  const address = useAddress();
+  const connect = useMetamask();
+
+  const publishCampaign = async (form) => {
+    try {
+      const data = await createCampaign([
+        address,
+        form.title,
+        form.description,
+        form.target,
+        new Date(form.deadline).getTime(),
+        form.image,
+      ]);
+      console.log("contract called", data);
+    } catch (error) {
+      console.log("contract failed", error);
+    }
+  };
+
+  return (
+    <StateContext.Provider
+      value={{
+        address,
+        contract,
+        createCampaign: publishCampaign,
+      }}
+    >
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
